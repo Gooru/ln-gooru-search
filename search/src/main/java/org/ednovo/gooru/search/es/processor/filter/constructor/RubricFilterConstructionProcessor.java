7efbe294004bb7444b48969960301acc2699d58c
Renu <100067@bq-067.bambeeq.com>
2017-05-22 15:35:18 +0530

    Improvements to rubric search

--- search/src/main/java/org/ednovo/gooru/search/es/processor/filter/constructor/RubricFilterConstructionProcessor.java
+++ search/src/main/java/org/ednovo/gooru/search/es/processor/filter/constructor/RubricFilterConstructionProcessor.java
@@ -13,16 +13,8 @@ public class RubricFilterConstructionProcessor extends FilterConstructionProcess
 	@Override
 	public void process(SearchData searchData, SearchResponse<Object> response) {
 		super.process(searchData, response);
-		if (searchData != null && searchData.getFilters() != null) {
-			if (!(searchData.getFilters().containsKey(FLT_PUBLISH_STATUS) || searchData.getFilters().containsKey(FLT_IS_FEATURED))) {
+		if (((searchData != null && searchData.getFilters() != null) && (!searchData.getFilters().containsKey(FLT_PUBLISH_STATUS))) || (searchData == null || searchData.getFilters() == null)) {
 				searchData.putFilter(FLT_PUBLISH_STATUS, PublishedStatus.PUBLISHED.getStatus());
-			} else if (searchData.getFilters().containsKey(FLT_IS_FEATURED)) {
-				if(Boolean.valueOf(searchData.getFilters().get(FLT_IS_FEATURED).toString())) {
-					searchData.putFilter(FLT_PUBLISH_STATUS, PublishedStatus.FEATURED.getStatus());
-				} else {
-					searchData.putFilter(FLT_PUBLISH_STATUS, PublishedStatus.PUBLISHED.getStatus());
-				}
-			}
 		}
 		searchData.putFilter(FLT_TENANT_ID, StringUtils.join(searchData.getUserPermits(), ","));
 	}
