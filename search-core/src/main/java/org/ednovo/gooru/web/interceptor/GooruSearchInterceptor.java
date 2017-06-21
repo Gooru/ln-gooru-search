cc9a84dc956e43566ccde40673fad9237e6418ea
Renu <100067@bq-067.bambeeq.com>
2017-06-21 12:31:19 +0530

    Revert "V-number change"
    This reverts commit 0826865975622955be1411a7ba9e160d29ac8d00.

--- search-core/src/main/java/org/ednovo/gooru/web/interceptor/GooruSearchInterceptor.java
+++ search-core/src/main/java/org/ednovo/gooru/web/interceptor/GooruSearchInterceptor.java
@@ -85,7 +85,7 @@ public class GooruSearchInterceptor extends HandlerInterceptorAdapter {
 		SessionContextSupport.putLogParameter("user", user);
 
 		JSONObject version = new JSONObject();
-		version.put("logApi", "4.0");
+		version.put("logApi", "0.4");
 		SessionContextSupport.putLogParameter("version", version);
 		return true;
 	}
