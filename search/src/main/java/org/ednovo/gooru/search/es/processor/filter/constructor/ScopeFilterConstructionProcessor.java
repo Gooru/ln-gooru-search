09bdb1bc6e88b62156fc65a77d09be6330bcfffd
RenukaGurumurthy <renuka.g@bambeeq.com>
2019-05-03 20:12:44 +0530

    At mycontent scope include collaborated contents as well

--- search/src/main/java/org/ednovo/gooru/search/es/processor/filter/constructor/ScopeFilterConstructionProcessor.java
+++ search/src/main/java/org/ednovo/gooru/search/es/processor/filter/constructor/ScopeFilterConstructionProcessor.java
@@ -35,7 +35,12 @@ public class ScopeFilterConstructionProcessor extends FilterConstructionProcesso
 		switch (target) {
 		case "my-content":
 			if (!searchData.getFilters().containsKey(AMPERSAND_CREATOR_ID)) {
-				searchData.putFilter("&^creator.userId", searchData.getUser().getGooruUId());
+				searchData.putFilter("&^orFilters", "creator.userId:" + searchData.getUser().getGooruUId() + "|collaboratorIds:" + searchData.getUser().getGooruUId());
+			} else if (searchData.getFilters().containsKey(AMPERSAND_CREATOR_ID) && StringUtils.isNotBlank(searchData.getFilters().get(AMPERSAND_CREATOR_ID).toString())) {
+				searchData.putFilter("&^orFilters", "creator.userId:" + searchData.getFilters().get(AMPERSAND_CREATOR_ID).toString() 
+											      + "|collaboratorIds:" + searchData.getFilters().get(AMPERSAND_CREATOR_ID).toString());
+				searchData.getFilters().remove(AMPERSAND_CREATOR_ID);
+				searchData.getParameters().remove(FLT_CREATOR_ID);
 			}
 			break;
 		case "tenant-library":
